490c3eb5eb89b8cfe54fc0fa66f62996a04d4776
Mail0rr <Vvorlovv7799@gmail.com>
2025-03-01 21:33:36 +0200

    Project

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from dishes import dishes
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html", dishes=dishes)
+
+@app.route("/dish/<int:dish_id>/")
+def dish_detail(dish_id):
+    dish = next((d for d in dishes if d["id"] == dish_id), None)
+    if not dish:
+        return "Блюдо не найдено", 404
+    return render_template("dish.html", dish=dish)
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
---
+++ dishes.py
@@ -0,0 +1,18 @@
+dishes = [
+    {
+        "id": 1,
+        "name": "Пепперони",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8g-VTSZiDaSqkheMYrjZZx3X_RjWk4BPa6A&s",
+        "description": "Острая пицца с пепперони и сыром",
+        "price": 250,
+        "ingredients": ["Тесто", "Соус", "Сыр", "Пепперони"]
+    },
+    {
+        "id": 2,
+        "name": "Маргарита",
+        "image": "https://lh3.googleusercontent.com/-F7-f2RyixFJ_0-MIGehlz7lp08CkWuy7Y64qDx8zcSrAyHA_uWVnJx1XOVAHg_qoFD7fW34aWScKlOz7tlHx8LeBxDoB64vaZ6LCKKMAPPnr8-QTpPpQVVK-xGPWFZomSVkVZXW",
+        "description": "Классическая пицца с томатами и базиликом",
+        "price": 220,
+        "ingredients": ["Тесто", "Соус", "Сыр", "Базилик"]
+    }
+]
