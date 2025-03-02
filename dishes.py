d9d7b2c02fc7d8e97f66e8278d57dc4fe20f7171
Mail0rr <Vvorlovv7799@gmail.com>
2025-03-02 16:25:12 +0200

    Project

--- dishes.py
+++ dishes.py
@@ -1,18 +1,194 @@
 dishes = [
     {
         "id": 1,
-        "name": "Пепперони",
-        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8g-VTSZiDaSqkheMYrjZZx3X_RjWk4BPa6A&s",
-        "description": "Острая пицца с пепперони и сыром",
-        "price": 250,
-        "ingredients": ["Тесто", "Соус", "Сыр", "Пепперони"]
+        "name": "Рамен",
+        "image": "https://static.insales-cdn.com/images/articles/1/1729/6629057/delicious-ramen-on-dark-surface.jpg",
+        "description": "Японский суп с пшеничной лапшой, мясом и овощами.",
+        "price": 300,
+        "ingredients": ["Лапша", "Бульон", "Свинина", "Яйцо", "Зелёный лук"]
     },
     {
         "id": 2,
-        "name": "Маргарита",
-        "image": "https://lh3.googleusercontent.com/-F7-f2RyixFJ_0-MIGehlz7lp08CkWuy7Y64qDx8zcSrAyHA_uWVnJx1XOVAHg_qoFD7fW34aWScKlOz7tlHx8LeBxDoB64vaZ6LCKKMAPPnr8-QTpPpQVVK-xGPWFZomSVkVZXW",
-        "description": "Классическая пицца с томатами и базиликом",
-        "price": 220,
-        "ingredients": ["Тесто", "Соус", "Сыр", "Базилик"]
+        "name": "Суши",
+        "image": "https://noa.ua/wp-content/uploads/2023/11/kombo-sajt-eksp.jpg",
+        "description": "Рис с морепродуктами, завернутый в нори.",
+        "price": 400,
+        "ingredients": ["Рис", "Рыба", "Нори", "Соевый соус", "Имбирь"]
+    },
+    {
+        "id": 3,
+        "name": "Фо Бо",
+        "image": "https://dishmagazine.com/wp-content/uploads/2024/11/pho-bo-soup-1300x1234.jpg",
+        "description": "Вьетнамский суп с рисовой лапшой и говядиной.",
+        "price": 280,
+        "ingredients": ["Бульон", "Рисовая лапша", "Говядина", "Лук", "Кинза"]
+    },
+    {
+        "id": 4,
+        "name": "Пад Тай",
+        "image": "https://dopuza.ru/wp-content/uploads/2023/08/lapsha-pad-taj-s-kuricej.jpg",
+        "description": "Тайская жареная лапша с креветками и орехами.",
+        "price": 320,
+        "ingredients": ["Лапша", "Креветки", "Яйцо", "Арахис", "Лайм"]
+    },
+    {
+        "id": 5,
+        "name": "Гёдза",
+        "image": "https://primemeat.ru/upload/webp/iblock/c76/shnuqpdheplq3ouqvjwi3w1u50z0ytse/gyoza.webp",
+        "description": "Японские жареные пельмени с мясом и овощами.",
+        "price": 250,
+        "ingredients": ["Тесто", "Свинина", "Капуста", "Чеснок", "Имбирь"]
+    },
+    {
+        "id": 6,
+        "name": "Том Ям",
+        "image": "https://the-challenger.ru/wp-content/uploads/2021/06/depositphotos_41260071_s-2019-600x600.jpeg",
+        "description": "Острый тайский суп с креветками и кокосовым молоком.",
+        "price": 350,
+        "ingredients": ["Бульон", "Креветки", "Лемонграсс", "Кокосовое молоко", "Чили"]
+    },
+    {
+        "id": 7,
+        "name": "Кимчи",
+        "image": "https://static.1000.menu/img/content-v2/93/46/63146/kimchi-zakaz_1646132417_8_max.jpg",
+        "description": "Острая корейская квашеная капуста.",
+        "price": 150,
+        "ingredients": ["Капуста", "Чеснок", "Имбирь", "Перец", "Рыбный соус"]
+    },
+    {
+        "id": 8,
+        "name": "Бибимбап",
+        "image": "https://socportal.info/site/assets/files/0/05/36/323/bibimbap-92850-1.780x480.jpeg?28xqvv",
+        "description": "Корейское блюдо с рисом, овощами и яйцом.",
+        "price": 280,
+        "ingredients": ["Рис", "Говядина", "Яйцо", "Шпинат", "Гочучжан"]
+    },
+    {
+        "id": 9,
+        "name": "Терияки",
+        "image": "https://www.chefmarket.ru/blog/wp-content/uploads/2020/10/teriyaki-chicken-1-2000x1200.jpg",
+        "description": "Курица в сладко-солёном соусе терияки.",
+        "price": 270,
+        "ingredients": ["Курица", "Соус терияки", "Сахар", "Имбирь", "Чеснок"]
+    },
+    {
+        "id": 10,
+        "name": "Димсам",
+        "image": "https://assets.resto.ru/data/msk/documents/53664/d059.jpg",
+        "description": "Китайские паровые закуски с различными начинками.",
+        "price": 260,
+        "ingredients": ["Тесто", "Свинина", "Креветки", "Лук", "Имбирь"]
+    },
+    {
+        "id": 11,
+        "name": "Чау Мейн",
+        "image": "https://static.insales-cdn.com/r/fKhEAS2j0ZI/rs:fit:1000:1000:1/plain/images/products/1/5849/896308953/1404429319.jpg@jpg",
+        "description": "Жареная китайская лапша с мясом и овощами.",
+        "price": 290,
+        "ingredients": ["Лапша", "Курица", "Морковь", "Лук", "Соевый соус"]
+    },
+    {
+        "id": 12,
+        "name": "Тофу Ма По",
+        "image": "https://avatars.dzeninfra.ru/get-zen_doc/1590748/pub_64fdf6362d85f43c1e580f15_64fdf6905921b358fd1fdcf9/scale_1200",
+        "description": "Китайское блюдо из тофу с пряным соусом.",
+        "price": 250,
+        "ingredients": ["Тофу", "Свинина", "Чили", "Чеснок", "Перец"]
+    },
+    {
+        "id": 13,
+        "name": "Спринг-роллы",
+        "image": "https://topfood.club/uploads/cropped/recipes/2022-05-23-8gtl50-gotovye-spring-rolly_1702286867.jpg",
+        "description": "Рисовые рулетики с креветками и овощами.",
+        "price": 230,
+        "ingredients": ["Рисовая бумага", "Креветки", "Огурец", "Листья салата", "Мята"]
+    },
+    {
+        "id": 14,
+        "name": "Суп Мисо",
+        "image": "https://img.povar.ru/mobile/24/fc/dd/a4/yaponskii_sup_miso-651768.JPG",
+        "description": "Японский суп на основе пасты мисо.",
+        "price": 200,
+        "ingredients": ["Бульон", "Мисо", "Тофу", "Вакаме", "Зелёный лук"]
+    },
+    {
+        "id": 15,
+        "name": "Бан Ми",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU8ImrQ6nAEa2jdJ2ybLswGxUDhajHdYNnzA&s",
+        "description": "Вьетнамский сэндвич с мясом и овощами.",
+        "price": 260,
+        "ingredients": ["Багет", "Свинина", "Морковь", "Кинза", "Соус"]
+    },
+    {
+        "id": 16,
+        "name": "Удон",
+        "image": "https://topfood.club/uploads/cropped/2023-11-01-6lpqjn-delicious-asian-udon-dish-with-vegetables-mushroo-2023-06-29-18-34-41-utc_1698834156.jpg",
+        "description": "Японская толстая лапша с бульоном и начинками.",
+        "price": 270,
+        "ingredients": ["Лапша удон", "Бульон", "Темпура", "Зелёный лук", "Водоросли"]
+    },
+    {
+        "id": 17,
+        "name": "Тяхан",
+        "image": "https://the-challenger.ru/wp-content/uploads/2020/08/tahan_3-800x545.jpg",
+        "description": "Жареный рис с овощами и мясом по-японски.",
+        "price": 240,
+        "ingredients": ["Рис", "Яйцо", "Лук", "Соевый соус", "Свинина"]
+    },
+    {
+        "id": 18,
+        "name": "Кунг Пао",
+        "image": "https://cdn.lifehacker.ru/wp-content/uploads/2024/07/105_1720014494.jpg",
+        "description": "Китайская курица с арахисом и острым соусом.",
+        "price": 280,
+        "ingredients": ["Курица", "Арахис", "Чили", "Чеснок", "Соевый соус"]
+    },
+    {
+        "id": 19,
+        "name": "Чапче",
+        "image": "https://kimchicafe.ru/wp-content/uploads/2020/01/chapche-1024x797.jpg",
+        "description": "Корейская лапша из крахмала с овощами и мясом.",
+        "price": 290,
+        "ingredients": ["Лапша", "Говядина", "Шпинат", "Перец", "Соевый соус"]
+    },
+    {
+        "id": 20,
+        "name": "Наси Горенг",
+        "image": "https://avatars.dzeninfra.ru/get-zen_doc/10148438/pub_64b29a060adc8743db4964bc_64b29ba6cd6be910996447be/scale_1200",
+        "description": "Индонезийский жареный рис с яйцом и специями.",
+        "price": 270,
+        "ingredients": ["Рис", "Яйцо", "Лук", "Чили", "Креветки"]
+    },
+    {
+        "id": 21,
+        "name": "Карри Кацу",
+        "image": "https://avatars.dzeninfra.ru/get-zen_doc/5289592/pub_6442a1b94f7f264a5a1d9a74_6442a242c7a9750d22860bf4/scale_1200",
+        "description": "Японская котлета с рисом и густым карри.",
+        "price": 310,
+        "ingredients": ["Свинина", "Карри", "Рис", "Панировка", "Лук"]
+    },
+    {
+        "id": 22,
+        "name": "Якитори",
+        "image": "https://brandfood.net/wp-content/uploads/2021/08/govyadina-yakitori-1.jpeg",
+        "description": "Японские куриные шашлычки на гриле.",
+        "price": 260,
+        "ingredients": ["Курица", "Соевый соус", "Мирин", "Чеснок", "Лук"]
+    },
+    {
+        "id": 23,
+        "name": "Гунбао Тофу",
+        "image": "https://scontent.fiev24-1.fna.fbcdn.net/v/t1.6435-9/86671076_497434767622485_696872453959319552_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=103&ccb=1-7&_nc_sid=0b6b33&_nc_ohc=3i06wNVA6wYQ7kNvgH1IOI_&_nc_oc=Adi46yIIWUvg9XRDJl9pPeFVwrluayrgBI_IVf257yeSvv_lpV7T5dCN7d0H2RDK-Vk&_nc_zt=23&_nc_ht=scontent.fiev24-1.fna&_nc_gid=AgH7xTu6jF9QnZqZUoBqPUJ&oh=00_AYC2582YmFdi2uRRXJfL2BA4EpFLY9DRHLrQR1RF4QYpqA&oe=67EBB008",
+        "description": "Китайское пряное блюдо с тофу и арахисом.",
+        "price": 250,
+        "ingredients": ["Тофу", "Чили", "Арахис", "Чеснок", "Соевый соус"]
+    },
+    {
+        "id": 24,
+        "name": "Сяо Лун Бао",
+        "image": "https://img.freepik.com/premium-photo/steamed-pork-soup-dumplings-named-xiao-long-bao-taiwan_315337-7282.jpg",
+        "description": "Китайские суповые пельмени на пару.",
+        "price": 270,
+        "ingredients": ["Тесто", "Свинина", "Бульон", "Имбирь", "Зелёный лук"]
     }
 ]
